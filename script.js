32bda9c9ac07886f00f044a6dbd77589e99e321c
manojna18 <manogna.kalyani@gmail.com>
2024-01-18 15:41:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+(function(){
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADASS';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
+
+let hue = 0;
+let direction = 0;
+
+let isDrawing = false;
+
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+  if(!isDrawing) {
+    return;
+  }
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+  hue++;
+  if(hue >= 360){
+    hue = 0;
+  }
+
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+    direction = !direction;
+  }
+
+  if(direction){
+    ctx.lineWidth++;
+  }
+  else{
+    ctx.lineWidth--;
+  }
+  
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', function(e){
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mouseup', function(){isDrawing = false;});
+canvas.addEventListener('mouseout', function(){isDrawing = false;});
+
+})();
